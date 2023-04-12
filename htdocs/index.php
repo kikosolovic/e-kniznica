c90a913ce5b28b30f2b46ad3d7607a2cb31bce72
Thomas <48056564+TheTomik1@users.noreply.github.com>
2023-04-12 11:02:29 +0200

    Add everything into temp folder

---
+++ htdocs/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Library</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Knižnica</a>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Uživatel</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Wishlist</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav me-auto mb-2 mb-md-0 ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="login/login.php">Prihlásiť</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="register/register.php">Registrovať</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav><br><br><br><br>
+
+<form method="post">
+<div class="row d-flex justify-content-center container">
+  <div>
+    <input class="form-control" type="text" name="author_or_book" aria-label="Search">
+  </div>
+  <div>
+    <input class="btn btn-outline-secondary" type="submit" value="Hľadať"></input>
+  </div>
+</div>
+</form>
+
+<br>
+
+
+  <?php
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $database = "library";
+      
+      $conn = new mysqli($servername, $username, $password, $database);
+      
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      $get_search_result = $_POST["author_or_book"] ?? null;
+      $sql = "SELECT books.title, books.author_id, authors.fullname, books.description, books.image FROM books INNER JOIN authors ON books.author_id = authors.id WHERE title REGEXP '$get_search_result' OR fullname REGEXP '$get_search_result'";
+      $result = $conn->query($sql);
+
+      echo "<table border='1'><tr><th>Bookname</th><th>Author</th><th>Description</th><th>Image</th></tr>";
+      while ($row = $result->fetch_assoc()) {
+          echo "<td>" . $row['title'] . "</td>";
+          echo "<td>" . $row['fullname'] . "</td>";
+          echo "<td>" . $row['description'] . "</td>";
+          echo "<td><img style='display:block;' src=" . $row['image'] . " height=100px ></td>";
+          echo "</tr>";
+      }
+      echo "</table>";
+  ?>
+
+<footer class="bg-dark text-center text-lg-start fixed-bottom">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  <p class="text-white">Year: <span id="datetime"></span></p><script>var dt = new Date(); var y = dt.getFullYear();
+document.getElementById("datetime").innerHTML=y;</script>
+    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+</footer>
+</body>
+</html>
---
+++ htdocs/login/login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <div class="logindiv">
+     <form method="post">
+        <h2>Login</h2>
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
+        <label>User Name</label>
+        <input class="input"type="text" name="uname"><br>
+        <label>Password</label>
+        <input class="input" type="password" name="password"> 
+        <p>Nemas ucet? <a href="./../register/register.php">Registrovat</a></p>
+        <div class="nehehe">
+            <input class="loginbtn" type="submit" value="Login">
+        </div>
+        
+     </form>
+     </div>
+</body>
+</html>
+
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "library";
+        
+    $conn = new mysqli($servername, $username, $password, $database);
+        
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $get_mail = $_POST["uname"] ?? null;
+    $get_password = $_POST["password"] ?? null; 
+
+    if (isset($get_mail) && isset($get_password)) {
+        if ($get_mail !== "" && $get_password !== "") {
+            $sql = "SELECT * from credentials WHERE email_address = '$get_mail' AND password = '$get_password'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                  echo "<p>Login successful!</p>";
+                }
+              } else {
+                echo "<p>Login failed!</p>";
+              }
+        } else {
+            echo "<p>Všetky polia musia byť vyplnené.</p>";
+        };
+    };
+?>
