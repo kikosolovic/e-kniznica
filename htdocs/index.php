e6f9525ff3782d5b52404b14de87b70581b1669a
Thomas <48056564+TheTomik1@users.noreply.github.com>
2023-04-26 11:20:45 +0200

    Added slider with live data from DB

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,81 +2,94 @@
 <html lang="en">
 <head>
     <title>Library</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="styles/styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Knižnica</a>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Uživatel</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Wishlist</a>
-        </li>
-      </ul>
-      <ul class="navbar-nav me-auto mb-2 mb-md-0 ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="login/login.php">Prihlásiť</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="register/register.php">Registrovať</a>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">Knižnica</a>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Uživatel</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Wishlist</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-md-0 ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="login/login.php">Prihlásiť</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="register/register.php">Registrovať</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav><br><br><br><br>
+
+  <form method="post">
+  <div class="row d-flex justify-content-center container">
+    <div>
+      <input class="form-control" type="text" name="author_or_book" aria-label="Search">
+    </div>
+    <div>
+      <input class="btn btn-outline-secondary" type="submit" value="Hľadať"></input>
     </div>
   </div>
-</nav><br><br><br><br>
+  </form>
 
-<form method="post">
-<div class="row d-flex justify-content-center container">
-  <div>
-    <input class="form-control" type="text" name="author_or_book" aria-label="Search">
-  </div>
-  <div>
-    <input class="btn btn-outline-secondary" type="submit" value="Hľadať"></input>
-  </div>
-</div>
-</form>
+  <br>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "library";
+        
+        $conn = new mysqli($servername, $username, $password, $database);
+        
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
 
-<br>
+        $get_search_result = $_POST["author_or_book"] ?? null;
+        $sql = "SELECT books.id, books.title, books.author_id, authors.fullname, books.description, books.image, release_year FROM books INNER JOIN authors ON books.author_id = authors.id WHERE title REGEXP '$get_search_result' OR fullname REGEXP '$get_search_result'";
+        $result = $conn->query($sql);
+        $bookdata = array();
 
+        while ($row = $result->fetch_assoc()) {
+          $bookdata[] = $row;
+        }
+        
+        echo '<div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel">';
+        echo '<div class="carousel-inner">';
+        echo '<div class="carousel-item active">';
+        echo '<div class="card-wrapper container-sm d-flex  justify-content-around">';
 
-  <?php
-      $servername = "localhost";
-      $username = "root";
-      $password = "";
-      $database = "library";
-      
-      $conn = new mysqli($servername, $username, $password, $database);
-      
-      if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
+        foreach ($result as $book) {
+          echo '<div class="card pics_in_a_row" style="width: 11rem;border: none ; "><img src="'. $book['image'] . '" class="card-img-top img" alt="imagetop"><div class="card-body"><h5 class="card-title">'. $book['title'] .'</h5><p> - '. $book['fullname'] . ' <br> - '. $book['release_year'] . '</div></div>';
+          if ($book['id'] % 5 == 0) {
+            echo '<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">';
+            echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
+            echo '<span class="visually-hidden">Previous</span>';
+            echo '</button>';
+            echo '<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">';
+            echo '<span class="carousel-control-next-icon" aria-hidden="true"></span>';
+            echo '<span class="visually-hidden">Next</span>';
+            echo '</button>';
+            echo '</div></div>';
+            if ($book['id'] != 10) {
+              echo '<div class="carousel-item active">';
+              echo '<div class="card-wrapper container-sm d-flex  justify-content-around">';
+            }
+          }
+        }
+        echo '</div></div>';
+    ?>
 
-      $get_search_result = $_POST["author_or_book"] ?? null;
-      $sql = "SELECT books.title, books.author_id, authors.fullname, books.description, books.image FROM books INNER JOIN authors ON books.author_id = authors.id WHERE title REGEXP '$get_search_result' OR fullname REGEXP '$get_search_result'";
-      $result = $conn->query($sql);
-
-      echo "<table border='1'><tr><th>Bookname</th><th>Author</th><th>Description</th><th>Image</th></tr>";
-      while ($row = $result->fetch_assoc()) {
-          echo "<td>" . $row['title'] . "</td>";
-          echo "<td>" . $row['fullname'] . "</td>";
-          echo "<td>" . $row['description'] . "</td>";
-          echo "<td><img style='display:block;' src=" . $row['image'] . " height=100px ></td>";
-          echo "</tr>";
-      }
-      echo "</table>";
-  ?>
-
-<footer class="bg-dark text-center text-lg-start fixed-bottom">
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-  <p class="text-white">Year: <span id="datetime"></span></p><script>var dt = new Date(); var y = dt.getFullYear();
-document.getElementById("datetime").innerHTML=y;</script>
-    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-  </div>
-</footer>
 </body>
 </html>
