e98bb2912fdd5b2d5ff1e89a68cf4c0dfa318802
tomse <tomset111@gmail.com>
2023-06-18 18:59:06 +0200

    + Password must be 8 characters long at least and contain one number and one uppercase and lowercase character

--- htdocs/register/registerscript.php
+++ htdocs/register/registerscript.php
@@ -19,24 +19,43 @@ function register() {
     $get_password = $_POST["password"] ?? null;
     $get_passwordagain = $_POST["passwordagain"] ?? null;
 
-    if (isset($get_fname) && isset($get_lname) && isset($get_email) && isset($get_password) && isset($get_passwordagain)) {
-        if ($get_fname !== "" && $get_lname !== "" && $get_email !== "" && $get_password !== "" && $get_passwordagain !== "") {
-            $highest_id = 0;
+    if (
+        isset($get_fname) && isset($get_lname) && isset($get_email) &&
+        isset($get_password) && isset($get_passwordagain)
+    ) {
+        if (
+            $get_fname !== "" && $get_lname !== "" && $get_email !== "" &&
+            $get_password !== "" && $get_passwordagain !== ""
+        ) {
+            if (strlen($get_password) >= 8 && preg_match('/[0-9]/', $get_password) && preg_match('/[a-z]/', $get_password) && preg_match('/[A-Z]/', $get_password)) {
+                $highest_id = 1;
 
-            $sql_get_id = "SELECT MAX(id)+1 AS highest_id FROM users";
-            $result_get_id = $conn->query($sql_get_id);
-            if ($result_get_id->num_rows > 0) {
-                $row = $result_get_id->fetch_assoc();
-                $highest_id = $row["highest_id"];
+                $sql_get_id = "SELECT MAX(id)+1 AS highest_id FROM users";
+                $stmt_get_id = $conn->prepare($sql_get_id);
+                $stmt_get_id->execute();
+                $result_get_id = $stmt_get_id->get_result();
+
+                if ($result_get_id->num_rows > 0) {
+                    $row = $result_get_id->fetch_assoc();
+                    $highest_id = $row["highest_id"] ?? 1;
+                } else {
+                    echo "<p style='color: #d0a100; font-size: 2em'>Technický problém. Skúste sa registrovať neskôr.<p>";
+                }
+
+                $sql1 = "INSERT INTO users (id, firstname, lastname, credential_id, admin) VALUES (?, ?, ?, ?, 0)";
+                $stmt1 = $conn->prepare($sql1);
+                $stmt1->bind_param("isss", $highest_id, $get_fname, $get_lname, $highest_id);
+                $stmt1->execute();
+
+                $sql2 = "INSERT INTO credentials (id, email_address, password) VALUES (?, ?, ?)";
+                $stmt2 = $conn->prepare($sql2);
+                $stmt2->bind_param("iss", $highest_id, $get_email, $get_passwordagain);
+                $stmt2->execute();
+
+                echo "<p style='color: dodgerblue; font-size: 2em'>Registrácia úspešná.<p>";
             } else {
-                echo "<p style='color: #d0a100; font-size: 2em'>Technický problém. Skúste sa registrovať neskôr.<p>";
+                echo "<p style='color: #a300ff; font-size: 2em'>Heslo musí mať aspoň 8 znakov a obsahovať aspoň jedno číslo, veľké a malé písmeno.<p>";
             }
-
-            $sql1 = "INSERT INTO users VALUES ($highest_id, '$get_fname', '$get_lname', $highest_id, 0)";
-            $sql2 = "INSERT INTO credentials VALUES ($highest_id, '$get_email', '$get_passwordagain')";
-            $conn->query($sql1);
-            $conn->query($sql2);
-            echo "<p style='color: dodgerblue; font-size: 2em'>Registrácia úspešná.<p>";
         } else {
             echo "<p style='color: #a300ff; font-size: 2em'>Všetky polia musia byť vyplnené.<p>";
         }
